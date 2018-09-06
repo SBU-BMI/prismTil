bbdc98a9357720947376748e12ccf0188b2cf7e9
Shayan Ray <shray@cs.stonybrook.edu>
2018-09-05 22:17:09 -0400

    Initial OpenSeaDragon Impl

---
+++ scripts/til.js
@@ -0,0 +1,21 @@
+var viewer = OpenSeadragon({
+    id: 'openseadragon1',
+    showNavigator:  true,
+     tileSources:   {
+        Image: {
+            xmlns:    "http://schemas.microsoft.com/deepzoom/2008",
+            Url:      "https://openseadragon.github.io/example-images/highsmith/highsmith_files/",
+            Format:   "jpg", 
+            Overlap:  "2", 
+            TileSize: "256",
+            ServerFormat: 'Default',
+            Size: {
+                Height: "9221",
+                Width:  "7026"
+            }
+        }
+    }
+});
+
+//const annotations = new OpenSeadragon.Annotations({ viewer });
+viewer.initializeAnnotations();
